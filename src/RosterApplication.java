94736d2a46a4696cb4c88aeaf1a775944a8fd652
MousseKwok <guo26x@mtholyoke.edu>
2017-10-03 15:23:11 -0400

    Update RosterApplication.java

--- src/RosterApplication.java
+++ src/RosterApplication.java
@@ -11,19 +11,14 @@ public class RosterApplication {
 	 * @param args a String type array
 	 */
 	public static void main(String[] args) {
-		//Create a new JFrame to display the game
 		JFrame frame = new JFrame("Roster of Data Structure");
 		
-		//Set the size of the frame
 		frame.setSize(1000, 800);
 		
-		//Add new UnrestrictedGuessingGameController to the frame
 		frame.add(new RosterController());
 	    
-		//Sets the operation that will happen by default when the user initiates a "close" on this frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Make the frame visible
+	
 		frame.setVisible(true);
 	}
 }
